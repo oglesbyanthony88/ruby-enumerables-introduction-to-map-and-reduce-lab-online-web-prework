b802ac1e2a589dc37c74758f9ffe08cd22eb175f
Anthony Oglesby <ogle1ant@gmail.com>
2019-08-09 17:00:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -69,13 +69,12 @@ end
 def reduce_to_any_true(source_array)
   i = 0
   while i < source_array.length do
-    if source_array[i]
-      return true
-    else 
+    if !source_array[i]
       return false
     end
     i += 1
   end
+  return true
 end
 
 
